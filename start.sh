8410b9b5429d33d3acae87999fd26cd72ea7b483
karunru <10268578+karunru@users.noreply.github.com>
2021-04-01 19:23:38 +0900

    add seed averagings

--- start.sh
+++ start.sh
@@ -12,6 +12,8 @@ cp -r ../ml-competition-template/config/examples config/
 cp -r ../ml-competition-template/input/* input/
 cp -r ../ml-competition-template/src ./
 cp ../ml-competition-template/main.py main.py
+cp ../ml-competition-template/seed_averaging.py seed_averaging.py
+cp ../ml-competition-template/make_seed_average_configs.py make_seed_average_configs.py
 cp ../ml-competition-template/.dockerignore .dockerignore
 cp ../ml-competition-template/.gitignore .gitignore
 cp ../ml-competition-template/Dockerfile Dockerfile
